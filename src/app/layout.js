caee9a352036b2555d360cb8ccf517722b2ebc80
Moin Khan <moinkhan1119rja@gmail.com>
2026-04-28 16:26:41 +0100

    project ceated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import NavBar from "@/components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +22,19 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
+      data-theme="light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-screen flex flex-col max-w-470 mx-auto ">
+        <Header />
+        <NavBar />
+        <main
+          className="w-11/12 mx-auto
+        "
+        >
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
